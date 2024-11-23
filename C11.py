241ecf0bbacd4381b9054a42a5c13a77d29df0d7
DanielGreber22 <danielgreber22@yahoo.com>
2024-11-23 14:02:21 +0100

    Update C11.py

--- C11.py
+++ C11.py
@@ -7,7 +7,7 @@ CENTRE_X = WIDTH / 2
 CENTRE_Y = HEIGHT / 2
 CENTRE = (CENTRE_X, CENTRE_Y)
 FINAL_LEVEL = 10
-START_SPEED = 10
+START_SPEED = 12
 ITEMS = ["bag","battery","bottle","chips"]
 
 game_over = False
@@ -22,9 +22,9 @@ def draw():
     screen.clear()
     screen.blit("backimg",(0,0))
     if game_over:
-        display_message("GAME OVER")
+        display_message("GAME OVER","try again")
     elif game_complete:  
-        display_message("you won")
+        display_message("YOU WON","well done")
     else:
         for item in items:
             item.draw()
@@ -56,11 +56,51 @@ def create_items(items_to_create):
         new_items.append(item)
     return new_items     
 
-def  layout_items(items_to_layout):
+def layout_items(items_to_layout):
     number_of_gaps = len(items_to_layout) +1
     gap_size = WIDTH/number_of_gaps
     random.shuffle(items_to_layout)
     for index,item in enumerate(items_to_layout):
         new_x_pos = (index +1)*gap_size
-        item.x = new_x_pos
-    
+        item.x = new_x_pos 
+
+def animate_items(items_to_animate):
+    global animations
+    for item in items_to_animate:
+        duration = START_SPEED - current_level
+        item.anchor = ("center","bottom")
+        animation = animate(item,duration = duration,on_finished =handle_game_over,y = HEIGHT)   
+
+def handle_game_over():
+    global game_over
+    game_over = True
+
+def on_mouse_down(pos):
+    global items,current_level
+    for item in items:
+        if item.collidepoint(pos):
+            if "paper" in item.image:
+                handle_game_complete()
+            else:
+                handle_game_over()
+
+def handle_game_complete():
+    global current_level,items,animations,game_complete
+    stop_animations(animations)
+    if current_level == FINAL_LEVEL:
+        game_complete = True
+    else:
+        current_level += 1
+        items = []
+        animations = []
+
+def stop_animations(animations_to_stop):
+    for animation in animations_to_stop:
+        if animation.running:
+            animation.stop()
+
+def display_message(heading_text, sub_heading_text):
+    screen.draw.text(heading_text, fontsize=60, center=CENTRE, color="white")
+    screen.draw.text(sub_heading_text, fontsize=30, center=(CENTRE_X, CENTRE_Y + 30), color="white")
+
+pgzrun.go()           
